5f30ab465f282e68929365f2f72103d37f10192c
ksdyb.park <ksdyb@amazon.com>
2022-05-01 20:57:14 +0900

    update

--- s3-trigger-scheduler/cdkscheduler/repositories/lambda-for-s3-trigger/index.js
+++ s3-trigger-scheduler/cdkscheduler/repositories/lambda-for-s3-trigger/index.js
@@ -7,7 +7,7 @@ exports.handler = async (event, context) => {
     console.log('## ENVIRONMENT VARIABLES: ' + JSON.stringify(process.env))
     console.log('## EVENT: ' + JSON.stringify(event))
 
-    // Get the object from the event and show its content type
+    // Get the object from the event
     const bucket = event.Records[0].s3.bucket.name;
     const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
     const eventInfo = {
